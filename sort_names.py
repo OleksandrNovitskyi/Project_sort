7635f6b44df13256de3ca4f13f9f748f056726dc
OleksandrNovitskyi <novitskyi.o.s@gmail.com>
2022-09-26 20:36:26 +0300

    corrected acording to the comments

--- sort_names.py
+++ sort_names.py
@@ -1,33 +1,56 @@
 import csv
+from fileinput import filename
 from deepface import DeepFace
+import sys
+import time
+
+
+filename = sys.argv[1]
+
+# ---- INPUT PARAMETERS ----
+limit_age = 25  # not younger than
+races = [
+    "black",
+    "white",
+    "asian",
+]  # enter necessary races. List of possible races - ['asian', 'indian', 'black', 'white', 'middle eastern', 'latino hispanic']
+forbidden_word = "NFT"  # a forbidden word in the current position of person
+black_list = "Indusi.csv"  # Filter by name. CSV file with two columns - First_name and Second_name
 
 
 def get_short_list(filename, first_names, last_names):
     """Read .csv file and create(rewrite) another .csv file with short list of people"""
     count = 0
-    forbidden_word = "NFT"
+    count_row = 0
+    res_file_name = filename[:-4] + "_filt.csv"
     with open(filename, "r", encoding="utf8") as file, open(
-        "result.csv", "w+", encoding="utf8", newline=""
+        res_file_name, "w+", encoding="utf8", newline=""
     ) as res_file:
         writer = csv.writer(res_file, delimiter=";")
-        csvreader = csv.reader(file, delimiter=";")
+        csvreader = csv.reader(
+            file, delimiter=","
+        )  # delimiter="," - if the data is concatenated in the first column by ','
+        # delimiter=";" - if the data in the different column
         row1 = next(csvreader)
         writer.writerow(row1)
         for row in csvreader:
+            count_row += 1
             if (
                 (row[24] not in first_names)
                 and (row[25] not in last_names)
                 and (forbidden_word not in row[43])
-                and face_filter(row[30])
+                and face_filter(row[30], limit_age, races)
             ):
                 writer.writerow(row)
                 count += 1
-        print("Done, now there are", count, "person")
+        print("There were", count_row, "people before filtering")
+        print("Done, now there are", count, "people")
+        print("Results at the file '{}'".format(res_file_name))
 
 
 def input_black_list():
     """Read .csv black list and return list of first_names and list of last_names"""
-    with open("Indusi.csv", "r", encoding="utf8") as file:
+    with open(black_list, "r", encoding="utf8") as file:
         csvreader = csv.reader(file, delimiter=",")
         first_names = []
         last_names = []
@@ -47,19 +70,17 @@ def face_filter(img, age=25, race="white"):
     """
     try:
         obj = DeepFace.analyze(img_path=img, actions=["age", "race"])
-        return (obj["age"] > age) and (obj["dominant_race"] == race)
+        return (obj["age"] > age) and (obj["dominant_race"] in races)
     except Exception as _ex:
         return _ex
 
 
-# print(face_verify("./media test/1516246784584.jfif"))
-
-
 def main():
 
-    filename = "test.csv"
+    start_time = time.time()
     first_names, last_names = input_black_list()
     get_short_list(filename, first_names, last_names)
+    print("Work time --- {} seconds ---".format(time.time() - start_time))
 
 
 if __name__ == "__main__":
