61e55775252ab8e2ebf405e3e844c18f351b2e7f
OleksandrNovitskyi <novitskyi.o.s@gmail.com>
2022-09-29 12:52:43 +0300

    work, but not quick

--- sort_names.py
+++ sort_names.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import time
+import pandas as pd
 import multiprocessing as mp
 from fileinput import filename
 from multiprocessing import Pool
@@ -16,7 +17,6 @@ races = [
     "asian",
 ]  # enter necessary races. List of possible races - ['asian', 'indian', 'black', 'white', 'middle eastern', 'latino hispanic']
 forbidden_word = "NFT"  # a forbidden word in the current position of person
-black_list = "Indusi.csv"  # Filter by name. CSV file with two columns - First_name and Second_name
 
 
 def read_file(filename):
@@ -26,63 +26,60 @@ def read_file(filename):
             file, delimiter=","
         )  # delimiter="," - if the data is concatenated in the first column by ','
         # delimiter=";" - if the data in the different column
-        read_file = list(csvreader)
-    return read_file
+        read_file_list = list(csvreader)
+    return read_file_list
 
 
-def create_result_file(filename, first_names, last_names):
+def create_result_file(filename, readed_list, first_names, last_names):
     """Create(rewrite) CSV file with short list of people"""
     res_file_name = filename[:-4] + "_filt.csv"
     with open(res_file_name, "w+", encoding="utf8", newline="") as res_file:
         writer = csv.writer(res_file, delimiter=";")
+        for row in readed_list:
+            if (
+                name_filter(row[24], first_names)
+                and (row[25] not in last_names)
+                and (forbidden_word not in row[43])
+                # and face_filter(row[30], limit_age, races)
+            ):
+                writer.writerow(row)
 
 
-def get_short_list(filename, first_names, last_names):
-    """Read .csv file and create(rewrite) another .csv file with short list of people"""
-    n_cond = 0
-    res_file_name = filename[:-4] + "_filt.csv"
-    with open(filename, "r", encoding="utf8") as file, open(
-        res_file_name, "w+", encoding="utf8", newline=""
-    ) as res_file:
-        writer = csv.writer(res_file, delimiter=";")
-        csvreader = csv.reader(
-            file, delimiter=","
-        )  # delimiter="," - if the data is concatenated in the first column by ','
-        # delimiter=";" - if the data in the different column
-        read_file = list(csvreader)
-        num_people = len(read_file) - 1
-        print("Estimated time ~ {} seconds".format(num_people * 1.2))
-        n = mp.cpu_count()
-        a = list(split(read_file, n))  # a == [[list1], [list2]...]
-        with Pool(n) as p:
-            p.map(
-                filter,
-                [
-                    (a[0], first_names, last_names, writer),
-                    (a[1], first_names, last_names, writer),
-                    (a[2], first_names, last_names, writer),
-                    (a[3], first_names, last_names, writer),
-                ],
-            )
-            # print(p.map(f, [1, 2, 3]))
-        # for row in read_file:
-        #     if (
-        #         (row[24] not in first_names)
-        #         and (row[25] not in last_names)
-        #         and (forbidden_word not in row[43])
-        #         # and face_filter(row[30], limit_age, races)
-        #     ):
-        #         writer.writerow(row)
-        #         count += 1
-        #     count_row += 1
-        #     condition = round(100 * count_row / num_people)
+# def get_short_list(filename, first_names, last_names):
+#     """Read .csv file and create(rewrite) another .csv file with short list of people"""
+#     n_cond = 0
+#     res_file_name = filename[:-4] + "_filt.csv"
+#     with open(filename, "r", encoding="utf8") as file, open(
+#         res_file_name, "w+", encoding="utf8", newline=""
+#     ) as res_file:
+#         writer = csv.writer(res_file, delimiter=";")
+#         csvreader = csv.reader(
+#             file, delimiter=","
+#         )  # delimiter="," - if the data is concatenated in the first column by ','
+#         # delimiter=";" - if the data in the different column
+#         read_file = list(csvreader)
+#         num_people = len(read_file) - 1
+#         print("Estimated time ~ {} seconds".format(num_people * 1.2))
 
-        #     if (condition % 3 == 0) and (n_cond != condition):
-        #         n_cond = condition
-        #         print("---- {}% completed ----".format(n_cond))
-        print("There were", num_people, "people before filtering")
-        # print("Done, now there are", count, "people")
-        print("Results at the file '{}'".format(res_file_name))
+#             # print(p.map(f, [1, 2, 3]))
+#         # for row in read_file:
+#         #     if (
+#         #         name_filter(row[24], first_names)
+#         #         and (row[25] not in last_names)
+#         #         and (forbidden_word not in row[43])
+#         #         # and face_filter(row[30], limit_age, races)
+#         #     ):
+#         #         writer.writerow(row)
+#         #         count += 1
+#         #     count_row += 1
+#         #     condition = round(100 * count_row / num_people)
+
+#         #     if (condition % 3 == 0) and (n_cond != condition):
+#         #         n_cond = condition
+#         #         print("---- {}% completed ----".format(n_cond))
+#         print("There were", num_people, "people before filtering")
+#         # print("Done, now there are", count, "people")
+#         print("Results at the file '{}'".format(res_file_name))
 
 
 def filter(read_file, first_names, last_names, writer):
@@ -90,7 +87,7 @@ def filter(read_file, first_names, last_names, writer):
     count_row = 0
     for row in read_file:
         if (
-            (row[24] not in first_names)
+            name_filter(row[24], first_names)
             and (row[25] not in last_names)
             and (forbidden_word not in row[43])
             # and face_filter(row[30], limit_age, races)
@@ -107,16 +104,27 @@ def filter(read_file, first_names, last_names, writer):
 
 
 def input_black_list():
-    """Read .csv black list and return list of first_names and list of last_names"""
-    with open(black_list, "r", encoding="utf8") as file:
-        csvreader = csv.reader(file, delimiter=",")
-        first_names = []
-        last_names = []
-        title_t = next(csvreader)
-        for row in csvreader:
-            first_names.append(row[0])
-            last_names.append(row[1])
+    """Read Google Sheet black list and return list of first_names and list of last_names
+
+    black_list = "https://docs.google.com/spreadsheets/d/1lXjDSK5398p1yJuTJn3vO4Ux9KDJtqP1esar1Z9U4vc/edit#gid=0"
+    """
+    sheet_id = "1lXjDSK5398p1yJuTJn3vO4Ux9KDJtqP1esar1Z9U4vc"
+    df = pd.read_csv(
+        f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+    )
+    first_names = df[df.columns[0]].values.tolist()
+    last_names = df[df.columns[1]].values.tolist()
     return first_names, last_names
+
+
+def name_filter(name, black_list):
+    """Filter people by name and by part of name"""
+    for word in black_list:
+        if len(word) <= 2:
+            return word != name
+        if word in name:
+            return False
+    return True
 
 
 def face_filter(img, age=25, races=["white"]):
@@ -144,7 +152,22 @@ def main():
     print("--- START ---")
     start_time = time.time()
     first_names, last_names = input_black_list()
-    get_short_list(filename, first_names, last_names)
+
+    read_file_list = read_file(filename)
+    # n = mp.cpu_count()
+    # a = list(split(read_file_list, n))  # a == [[list1], [list2]...]
+    create_result_file(filename, read_file_list, first_names, last_names)
+
+    # with Pool(n) as p:
+    #     p.map(
+    #         create_result_file,
+    #         [
+    #             (filename, a[0], first_names, last_names),
+    #             (filename, a[1], first_names, last_names),
+    #             (filename, a[2], first_names, last_names),
+    #             (filename, a[3], first_names, last_names),
+    #         ],
+    # )
     print("Work time --- {} seconds ---".format(time.time() - start_time))
 
 
